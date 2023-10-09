3d74ea25da4b582e645df239a6d8ef602235fb1e
PoteznyDominator <b.murawski00@gmail.com>
2023-10-09 21:34:59 +0200

    resolved lab01

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -1,17 +1,34 @@
 import pandas as pd
 
 class SampleBase:
-  sample_: list[list[str]] = []
-  attributes_: list[str] = []
-  is_attr_symb_: list[bool] = []
+  samples: list[list[str]] = []
+  attributes: list[str] = []
+  is_atr_char: list[bool] = []
 
   def __init__(self, value_path: str, attr_path: str):
-    df = pd.read_csv(value_path, sep='\t', header=None)
     df_attr = pd.read_csv(attr_path, sep='\t', header=None)
+    df = pd.read_csv(value_path, sep='\t', header=None, names=df_attr[0])
 
-    self.attributes_ = df_attr.columns
+    self.attributes = df_attr[0].tolist()
 
-    print(df_attr)
+    for _, row in df.iterrows():
+      self.samples.append(row.tolist())
+
+    for _, row in df_attr.iterrows():
+      self.is_atr_char.append(row[1] == "s")
+
+  def get_all_samples(self) -> list[list[str]]:
+    return self.samples
+
+  def get_sample(self, col: int, row: int) -> str:
+    return self.samples[row][col]
+
+  def get_all_attributes(self):
+    return self.is_atr_char
+
+  def is_attribute_char(self, index: int) -> bool:
+    return self.is_atr_char[index]
 
 
-SampleBase("iris.txt", "iris-type.txt")
+base = SampleBase("iris.txt", "iris-type.txt")
+print(base.get_sample(1,0))
