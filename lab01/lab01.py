18c59eea7e6acb4c40e68e102acf8bf336d6b4d9
PoteznyDominator <b.murawski00@gmail.com>
2023-10-14 09:00:58 +0200

    Resolved lab02

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -1,17 +1,19 @@
+import pandas
 import pandas as pd
 
 class SampleBase:
   samples: list[list[str]] = []
   attributes: list[str] = []
   is_atr_char: list[bool] = []
+  df: pandas.DataFrame
 
   def __init__(self, value_path: str, attr_path: str):
     df_attr = pd.read_csv(attr_path, sep='\t', header=None)
-    df = pd.read_csv(value_path, sep='\t', header=None, names=df_attr[0])
+    self.df = pd.read_csv(value_path, sep='\t', header=None, names=df_attr[0])
 
     self.attributes = df_attr[0].tolist()
 
-    for _, row in df.iterrows():
+    for _, row in self.df.iterrows():
       self.samples.append(row.tolist())
 
     for _, row in df_attr.iterrows():
@@ -21,14 +23,25 @@ class SampleBase:
     return self.samples
 
   def get_sample(self, col: int, row: int) -> str:
+    if row > len(self.samples) or col > len(self.samples[0]):
+      return "Index out of range"
+
     return self.samples[row][col]
 
   def get_all_attributes(self):
     return self.is_atr_char
 
-  def is_attribute_char(self, index: int) -> bool:
+  def is_attribute_char(self, index: int):
+    if index > len(self.is_atr_char):
+      return "Index out of range"
+
     return self.is_atr_char[index]
+
+  def get_df(self):
+    return self.df
 
 
 base = SampleBase("iris.txt", "iris-type.txt")
 print(base.get_sample(1,0))
+print(base.get_all_samples())
+print(base.get_df())
